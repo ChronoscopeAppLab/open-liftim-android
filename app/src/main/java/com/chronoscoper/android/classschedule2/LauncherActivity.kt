8a9ecdec2acff6e6c157d0ba16bc874bc1dc6875
Koki Fukuda <ko.fu.dev@gmail.com>
2017-12-09 02:37:10 +0900

    Add launcher entry

---
+++ app/src/main/java/com/chronoscoper/android/classschedule2/LauncherActivity.kt
@@ -0,0 +1,24 @@
+package com.chronoscoper.android.classschedule2
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.preference.PreferenceManager
+import com.chronoscoper.android.classschedule2.sync.SyncConfigurationActivity
+
+class LauncherActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this)
+        val setupCompleted = sharedPrefs.getBoolean(getString(R.string.p_setup_completed), false)
+        if (setupCompleted) {
+
+        } else {
+            startActivity(Intent(this, SyncConfigurationActivity::class.java))
+        }
+        finish()
+        overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
+    }
+}

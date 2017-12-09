da5586106d212a6d3b5687da7b1f5f0b698a8b6d
Koki Fukuda <ko.fu.dev@gmail.com>
2017-12-10 03:32:13 +0900

    Implement getting info of Liftim code

---
+++ app/src/main/java/com/chronoscoper/android/classschedule2/LiftimApplication.kt
@@ -0,0 +1,31 @@
+/*
+ * Copyright 2017 Chronoscope
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.chronoscoper.android.classschedule2
+
+import android.app.Application
+import android.preference.PreferenceManager
+import com.chronoscoper.android.classschedule2.sync.LiftimSyncEnvironment
+
+class LiftimApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+        val sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this)
+        LiftimSyncEnvironment.init(
+                this, sharedPrefs.getString(getString(R.string.p_sync_url),
+                "http://example.com/"))
+    }
+}
---
+++ sync/src/main/java/com/chronoscoper/android/classschedule2/sync/LiftimCodeInfo.java
@@ -0,0 +1,31 @@
+package com.chronoscoper.android.classschedule2.sync;
+
+import com.github.gfx.android.orma.annotation.Column;
+import com.github.gfx.android.orma.annotation.PrimaryKey;
+import com.github.gfx.android.orma.annotation.Setter;
+import com.github.gfx.android.orma.annotation.Table;
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+@Table("liftim_code_info")
+public class LiftimCodeInfo {
+    @Setter
+    public LiftimCodeInfo(long liftimCode, String name, String addDate) {
+        this.liftimCode = liftimCode;
+        this.name = name;
+        this.addDate = addDate;
+    }
+
+    @PrimaryKey
+    public long liftimCode;
+
+    @Column("name")
+    @Expose
+    @SerializedName("name")
+    public String name;
+
+    @Column("add_date")
+    @Expose
+    @SerializedName("addDate")
+    public String addDate;
+}

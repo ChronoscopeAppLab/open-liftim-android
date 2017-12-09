9447e5c1496a1a2f7be3a0f594d77ed099890f8a
kofu <ko.fu.dev@gmail.com>
2017-12-09 16:15:53 +0900

    Add Home activity

---
+++ app/src/main/java/com/chronoscoper/android/classschedule2/home/HomeActivity.kt
@@ -0,0 +1,12 @@
+package com.chronoscoper.android.classschedule2.home
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.chronoscoper.android.classschedule2.R
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}

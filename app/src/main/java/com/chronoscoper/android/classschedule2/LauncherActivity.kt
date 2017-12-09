56d57489ff17df8eb5dfd568c9c7faf73c6e14de
Koki Fukuda <ko.fu.dev@gmail.com>
2017-12-09 16:15:53 +0900

    Add Home activity

--- app/src/main/java/com/chronoscoper/android/classschedule2/LauncherActivity.kt
+++ app/src/main/java/com/chronoscoper/android/classschedule2/LauncherActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.preference.PreferenceManager
+import com.chronoscoper.android.classschedule2.home.HomeActivity
 import com.chronoscoper.android.classschedule2.sync.SyncConfigurationActivity
 
 class LauncherActivity : AppCompatActivity() {
@@ -14,7 +15,7 @@ class LauncherActivity : AppCompatActivity() {
         val sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this)
         val setupCompleted = sharedPrefs.getBoolean(getString(R.string.p_setup_completed), false)
         if (setupCompleted) {
-
+            startActivity(Intent(this, HomeActivity::class.java))
         } else {
             startActivity(Intent(this, SyncConfigurationActivity::class.java))
         }

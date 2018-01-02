cd847c26046013f90792364bec1791b687a9da21
kofu <ko.fu.dev@gmail.com>
2018-01-02 11:37:42 +0900

    Implement NavigationDrawer

--- app/src/main/java/com/chronoscoper/android/classschedule2/home/HomeActivity.kt
+++ app/src/main/java/com/chronoscoper/android/classschedule2/home/HomeActivity.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017 Chronoscope
+ * Copyright 2017-2018 Chronoscope
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -17,16 +17,18 @@ package com.chronoscoper.android.classschedule2.home
 
 import android.os.Bundle
 import android.support.design.widget.NavigationView
+import android.support.v4.app.Fragment
 import android.support.v4.widget.DrawerLayout
 import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.widget.Toolbar
+import android.view.Gravity
 import android.view.MenuItem
 import android.view.View
 import com.chronoscoper.android.classschedule2.BaseActivity
 import com.chronoscoper.android.classschedule2.R
 import kotterknife.bindView
 
-class HomeActivity : BaseActivity(), DrawerLayout.DrawerListener, NavigationView.OnNavigationItemSelectedListener {
+class HomeActivity : BaseActivity(), NavigationView.OnNavigationItemSelectedListener {
     private val drawer by bindView<DrawerLayout>(R.id.activity_home)
 
     private val drawerMenu by bindView<NavigationView>(R.id.drawer_menu)
@@ -36,10 +38,10 @@ class HomeActivity : BaseActivity(), DrawerLayout.DrawerListener, NavigationView
         setContentView(R.layout.activity_home)
 
         if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                    .replace(R.id.content, HomeFragment())
-                    .commit()
+            replaceFragment(HomeFragment())
         }
+
+        drawerMenu.setCheckedItem(R.id.drawer_timetable)
 
         val toolbar = findViewById<Toolbar>(R.id.toolbar)
         setSupportActionBar(toolbar)
@@ -47,22 +49,54 @@ class HomeActivity : BaseActivity(), DrawerLayout.DrawerListener, NavigationView
         val toggle = ActionBarDrawerToggle(this, drawer,
                 toolbar, R.string.open_drawer, R.string.close_drawer)
         drawer.addDrawerListener(toggle)
-        drawer.addDrawerListener(this)
         drawerMenu.setNavigationItemSelectedListener(this)
         toggle.syncState()
     }
 
-    override fun onDrawerStateChanged(newState: Int) {
+    private var contentFragment: Fragment? = null
+
+    private fun replaceFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+                .replace(R.id.content, fragment)
+                .commit()
+        contentFragment = fragment
     }
 
-    override fun onDrawerSlide(drawerView: View?, slideOffset: Float) {
+    fun onHomePageChanged(page: Int) {
+        when (page) {
+            0 -> {
+                drawerMenu.setCheckedItem(R.id.drawer_timetable)
+            }
+            1 -> {
+                drawerMenu.setCheckedItem(R.id.drawer_info)
+            }
+        }
     }
 
-    override fun onDrawerClosed(drawerView: View?) {
-    }
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.drawer_timetable -> {
+                if (contentFragment !is HomeFragment) {
+                    replaceFragment(HomeFragment())
+                }
+                val fragment = contentFragment as? HomeFragment ?: return true
+                fragment.page = 0
+            }
+            R.id.drawer_info -> {
+                if (contentFragment !is HomeFragment) {
+                    replaceFragment(HomeFragment())
+                }
+                val fragment = contentFragment as? HomeFragment ?: return true
+                fragment.page = 1
+            }
+            R.id.drawer_weekly -> {
 
-    override fun onDrawerOpened(drawerView: View?) {
-    }
+            }
+            R.id.drawer_settings -> {
 
-    override fun onNavigationItemSelected(item: MenuItem): Boolean = true
+            }
+        }
+        drawer.closeDrawer(Gravity.START)
+        return true
+    }
 }

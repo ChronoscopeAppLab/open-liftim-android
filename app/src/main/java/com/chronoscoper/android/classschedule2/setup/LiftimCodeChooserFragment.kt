a4158a1046f958a13150d75dd97a2d5ece1fe730
kofu <ko.fu.dev@gmail.com>
2018-01-08 01:33:33 +0900

    Choose default Liftim code at first

--- app/src/main/java/com/chronoscoper/android/classschedule2/setup/LiftimCodeChooserFragment.kt
+++ app/src/main/java/com/chronoscoper/android/classschedule2/setup/LiftimCodeChooserFragment.kt
@@ -15,7 +15,85 @@
  */
 package com.chronoscoper.android.classschedule2.setup
 
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import android.preference.PreferenceManager
 import android.support.v4.app.Fragment
+import android.support.v7.widget.DividerItemDecoration
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+import com.chronoscoper.android.classschedule2.R
+import com.chronoscoper.android.classschedule2.home.HomeActivity
+import com.chronoscoper.android.classschedule2.sync.LiftimCodeInfo
+import com.chronoscoper.android.classschedule2.sync.LiftimSyncEnvironment
+import com.chronoscoper.android.classschedule2.view.RecyclerViewHolder
+import kotterknife.bindView
 
 class LiftimCodeChooserFragment : Fragment() {
+    override fun onCreateView(inflater: LayoutInflater?,
+                              container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? =
+            inflater?.inflate(R.layout.fragment_liftim_code_chooser, container, false)
+
+    private val list by bindView<RecyclerView>(R.id.list)
+    private val joinButton by bindView<Button>(R.id.join)
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        list.adapter = LiftimCodeAdapter(activity)
+        list.addItemDecoration(DividerItemDecoration(context, DividerItemDecoration.VERTICAL))
+        joinButton.setOnClickListener {
+            //TODO
+        }
+    }
+
+    private class LiftimCodeAdapter(val activity: Activity) :
+            RecyclerView.Adapter<RecyclerViewHolder>() {
+        private val data = mutableListOf<LiftimCodeInfo>()
+
+        init {
+            data.addAll(LiftimSyncEnvironment.getOrmaDatabase()
+                    .selectFromLiftimCodeInfo().toList())
+        }
+
+        override fun onBindViewHolder(holder: RecyclerViewHolder?, position: Int) {
+            val view = holder?.itemView ?: return
+            val item = data[position]
+            val image = view.findViewById<ImageView>(R.id.image)
+            val name = view.findViewById<TextView>(R.id.liftim_code_name)
+            Glide.with(activity)
+                    .load(LiftimSyncEnvironment
+                            .getApiUrl("liftim_code_image.png?" +
+                                    "liftim_code=${item.liftimCode}&" +
+                                    "token=${LiftimSyncEnvironment.getToken()}"))
+                    .apply(RequestOptions.circleCropTransform())
+                    .into(image)
+            name.text = item.name
+            view.setOnClickListener {
+                PreferenceManager.getDefaultSharedPreferences(activity)
+                        .edit()
+                        .putLong(activity.getString(R.string.p_default_liftim_code), item.liftimCode)
+                        .apply()
+                activity.finish()
+                activity.startActivity(Intent(activity, HomeActivity::class.java)
+                        .addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK))
+            }
+        }
+
+        val inflater by lazy { LayoutInflater.from(activity) }
+
+        override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): RecyclerViewHolder =
+                RecyclerViewHolder(inflater.inflate(
+                        R.layout.liftim_code_chooser_item, parent, false))
+
+        override fun getItemCount(): Int = data.size
+    }
 }

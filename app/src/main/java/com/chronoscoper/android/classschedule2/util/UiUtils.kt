e1a7b2597c8a4e815ebc52ee986397ec027ef0c9
Koki Fukuda <ko.fu.dev@gmail.com>
2018-01-27 01:28:00 +0900

    Add better animation to ViewInfoActivity

--- app/src/main/java/com/chronoscoper/android/classschedule2/util/UiUtils.kt
+++ app/src/main/java/com/chronoscoper/android/classschedule2/util/UiUtils.kt
@@ -2,7 +2,9 @@ package com.chronoscoper.android.classschedule2.util
 
 import android.annotation.SuppressLint
 import android.content.Context
+import android.graphics.Color
 import android.graphics.drawable.Drawable
+import android.support.annotation.ColorInt
 import android.view.Gravity
 import android.view.LayoutInflater
 import android.widget.TextView
@@ -10,6 +12,7 @@ import android.widget.Toast
 import com.bumptech.glide.TransitionOptions
 import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
 import com.chronoscoper.android.classschedule2.R
+import com.chronoscoper.android.classschedule2.sync.Info
 import com.chronoscoper.android.classschedule2.view.ProgressiveFadeInFactory
 
 @SuppressLint("InflateParams")
@@ -25,4 +28,36 @@ fun showToast(context: Context, text: CharSequence, duration: Int) {
 
 fun progressiveFadeInTransition(): TransitionOptions<DrawableTransitionOptions, Drawable> {
     return DrawableTransitionOptions.with(ProgressiveFadeInFactory())
+}
+
+@ColorInt
+fun getColorForInfoType(type: Int): Int {
+    return when (type) {
+        Info.TYPE_UNSPECIFIED -> {
+            -0x777778
+        }
+        Info.TYPE_EVENT -> {
+            -0x6800
+        }
+        Info.TYPE_INFORMATION -> {
+            -0xde690d
+        }
+        Info.TYPE_SUBMISSION -> {
+            -0xb350b0
+        }
+        Info.TYPE_LOCAL_MEMO -> {
+            -0x98c549
+        }
+        else -> {
+            Color.BLACK
+        }
+    }
+}
+
+@ColorInt
+fun getDarkerColor(source: Int): Int {
+    val hsv = FloatArray(3)
+    Color.colorToHSV(source, hsv)
+    hsv[2] *= 0.8f
+    return Color.HSVToColor(hsv)
 }

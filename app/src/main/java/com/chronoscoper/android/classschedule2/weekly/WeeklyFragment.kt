d3366c8442f15fd523c185c71afaf28152a8a28a
kofu <ko.fu.dev@gmail.com>
2018-03-07 00:10:28 +0900

    Re-implement weekly timetable

--- app/src/main/java/com/chronoscoper/android/classschedule2/weekly/WeeklyFragment.kt
+++ app/src/main/java/com/chronoscoper/android/classschedule2/weekly/WeeklyFragment.kt
@@ -15,15 +15,11 @@
  */
 package com.chronoscoper.android.classschedule2.weekly
 
-import android.content.Context
 import android.graphics.PorterDuff
 import android.os.Bundle
-import android.support.design.widget.TabLayout
 import android.support.v4.app.Fragment
-import android.support.v4.app.FragmentManager
-import android.support.v4.app.FragmentStatePagerAdapter
-import android.support.v4.view.ViewPager
-import android.support.v7.widget.DividerItemDecoration
+import android.support.v4.content.ContextCompat
+import android.support.v7.widget.GridLayoutManager
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
@@ -32,8 +28,7 @@ import android.widget.TextView
 import com.chronoscoper.android.classschedule2.R
 import com.chronoscoper.android.classschedule2.sync.LiftimContext
 import com.chronoscoper.android.classschedule2.sync.WeeklyItem
-import com.chronoscoper.android.classschedule2.util.obtainColorCorrespondsTo
-import com.chronoscoper.android.classschedule2.view.RecyclerViewHolder
+import com.chronoscoper.android.classschedule2.view.ObservableScrollView
 import kotterknife.bindView
 
 class WeeklyFragment : Fragment() {
@@ -41,106 +36,61 @@ class WeeklyFragment : Fragment() {
             inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? =
             inflater.inflate(R.layout.fragment_weekly, container, false)
 
-    private val tabs by bindView<TabLayout>(R.id.tab)
-    private val pager by bindView<ViewPager>(R.id.pager)
+    private val indexHoriz by bindView<View>(R.id.index_horiz)
+    private val indexVert by bindView<ViewGroup>(R.id.index_vert)
+    private val grid by bindView<RecyclerView>(R.id.grid)
+    private val gridVertScroll by bindView<ObservableScrollView>(R.id.grid_horizontal_scroll)
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        pager.adapter = Adapter(context!!, childFragmentManager)
-        tabs.tabMode = TabLayout.MODE_SCROLLABLE
-        tabs.tabGravity = TabLayout.GRAVITY_FILL
-        tabs.setupWithViewPager(pager)
+
+        gridVertScroll.onScrollListener = {
+            indexVert.scrollTo(0, it)
+        }
+
+        grid.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+            override fun onScrolled(recyclerView: RecyclerView?, dx: Int, dy: Int) {
+                super.onScrolled(recyclerView, dx, dy)
+                indexHoriz.scrollTo(recyclerView!!.computeHorizontalScrollOffset(), 0)
+            }
+        })
+
+        val loadedData = LiftimContext.getOrmaDatabase().selectFromWeeklyItem()
+                .liftimCodeEq(LiftimContext.getLiftimCode())
+                .orderByLiftimCodeAsc()
+                .toList()
+        val data = arrayListOf<WeeklyItem?>(null, null, null, null, null, null, null)
+        var minMinIndex = Integer.MAX_VALUE
+        var rowCount = 0
+        loadedData.forEach {
+            if (it.subjects == null) {
+                it.subjects = LiftimContext.getGson()
+                        .fromJson(it.serializedSubjects, Array<String>::class.java)
+            }
+            if (it.minIndex < minMinIndex) minMinIndex = it.minIndex
+            if (it.subjects.size > rowCount) rowCount = it.subjects.size
+            if (it.dayOfWeek in 1..7) {
+                data[it.dayOfWeek - 1] = it
+            }
+        }
+        grid.layoutManager = GridLayoutManager(context, rowCount,
+                GridLayoutManager.HORIZONTAL, false)
+        grid.adapter = WeeklyTimetableAdapter(context!!, loadedData, minMinIndex, rowCount)
+        setupIndexVert(indexVert, minMinIndex, rowCount)
     }
 
-    class Adapter(private val context: Context,
-                  fm: FragmentManager) : FragmentStatePagerAdapter(fm) {
-        private val data = mutableListOf<WeeklyItem>()
+    private val inflater by lazy { LayoutInflater.from(context) }
 
-        init {
-            data.addAll(LiftimContext.getOrmaDatabase()
-                    .selectFromWeeklyItem()
-                    .liftimCodeEq(LiftimContext.getLiftimCode())
-                    .orderByDayOfWeekAsc())
-        }
-
-        override fun getPageTitle(position: Int): CharSequence? {
-            val dayOfWeek = data[position].dayOfWeek
-            return when (dayOfWeek) {
-                1 -> context.getString(R.string.day_of_week_sunday)
-                2 -> context.getString(R.string.day_of_week_monday)
-                3 -> context.getString(R.string.day_of_week_tuesday)
-                4 -> context.getString(R.string.day_of_week_wednesday)
-                5 -> context.getString(R.string.day_of_week_thursday)
-                6 -> context.getString(R.string.day_of_week_friday)
-                7 -> context.getString(R.string.day_of_week_saturday)
-                else -> null
-            }
-        }
-
-        override fun getItem(position: Int): Fragment = DayFragment.obtain(data[position].dayOfWeek)
-
-        override fun getCount(): Int = data.size
-
-        class DayFragment : Fragment() {
-            companion object {
-                private const val DAY_OF_WEEK = "day_of_week"
-                fun obtain(dayOfWeek: Int): DayFragment {
-                    val f = DayFragment()
-                    f.arguments = Bundle().apply {
-                        putInt(DAY_OF_WEEK, dayOfWeek)
-                    }
-                    return f
-                }
-            }
-
-            override fun onCreateView(
-                    inflater: LayoutInflater,
-                    container: ViewGroup?,
-                    savedInstanceState: Bundle?): View? =
-                    inflater.inflate(R.layout.fragment_day, container, false)
-
-            private val list by bindView<RecyclerView>(R.id.list)
-
-            override fun onActivityCreated(savedInstanceState: Bundle?) {
-                super.onActivityCreated(savedInstanceState)
-
-                list.adapter = DayAdapter(context!!, arguments!!.getInt(DAY_OF_WEEK))
-                list.addItemDecoration(DividerItemDecoration(context, DividerItemDecoration.VERTICAL))
-            }
-
-            class DayAdapter(context: Context, dayOfWeek: Int)
-                : RecyclerView.Adapter<RecyclerViewHolder>() {
-                private val data = LiftimContext.getOrmaDatabase()
-                        .selectFromWeeklyItem()
-                        .liftimCodeEq(LiftimContext.getLiftimCode())
-                        .dayOfWeekEq(dayOfWeek)
-                        .firstOrNull()
-                        ?.apply {
-                            subjects = LiftimContext.getGson()
-                                    .fromJson(serializedSubjects, Array<String>::class.java)
-                        } ?: WeeklyItem().apply { subjects = arrayOf() }
-
-                private val inflater by lazy { LayoutInflater.from(context) }
-
-                override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int):
-                        RecyclerViewHolder = RecyclerViewHolder(
-                        inflater.inflate(R.layout.weekly_day_item, parent, false))
-
-
-                override fun onBindViewHolder(holder: RecyclerViewHolder?, position: Int) {
-                    val view = holder?.itemView ?: return
-                    val subjectName = data.subjects[position]
-
-                    val subject = view.findViewById<TextView>(R.id.subject)
-                    subject.text = subjectName
-                    val index = view.findViewById<TextView>(R.id.index)
-                    index.text = (position + data.minIndex).toString()
-                    index.background.setColorFilter(
-                            obtainColorCorrespondsTo(subjectName), PorterDuff.Mode.SRC_IN)
-                }
-
-                override fun getItemCount(): Int = data.subjects.size
-            }
+    private fun setupIndexVert(container: ViewGroup, minMinIndex: Int, rowCount: Int) {
+        val zebra = arrayOf(ContextCompat.getColor(context!!, R.color.zebra_dark),
+                ContextCompat.getColor(context!!, R.color.zebra_light))
+        container.removeAllViews()
+        for (i in minMinIndex until minMinIndex + rowCount) {
+            val view = inflater.inflate(R.layout.weekly_index_vert,
+                    container, false) as TextView
+            view.text = i.toString()
+            view.background.setColorFilter(zebra[i % 2], PorterDuff.Mode.SRC)
+            container.addView(view)
         }
     }
 }

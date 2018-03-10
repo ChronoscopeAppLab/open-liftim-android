e61481b28f3d7f64f1d665107c3e812c8b0e8fa5
Koki Fukuda <ko.fu.dev@gmail.com>
2018-03-11 02:46:42 +0900

    Optimize popup menu

---
+++ app/src/main/java/com/chronoscoper/android/classschedule2/view/PopupMenuCompat.kt
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2018 Chronoscope
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.chronoscoper.android.classschedule2.view
+
+import android.content.Context
+import android.os.Build
+import android.support.annotation.MenuRes
+import android.view.Gravity
+import android.view.MenuItem
+import android.view.View
+import android.widget.PopupMenu
+
+class PopupMenuCompat(context: Context, private val anchor: View) {
+    private val popup by lazy {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP_MR1) {
+            PopupMenu(context, anchor, Gravity.NO_GRAVITY,
+                    0, android.R.style.Widget_Material_PopupMenu_Overflow)
+        } else {
+            PopupMenu(context, anchor)
+        }
+    }
+
+    fun inflate(@MenuRes menu: Int) {
+        popup.inflate(menu)
+    }
+
+    fun setOnMenuItemClickListener(l: (item: MenuItem) -> Boolean) {
+        popup.setOnMenuItemClickListener(l)
+    }
+
+    fun show() {
+        popup.show()
+    }
+
+    val dragToOpenListener: View.OnTouchListener?
+        get() = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            popup.dragToOpenListener
+        } else {
+            null
+        }
+}

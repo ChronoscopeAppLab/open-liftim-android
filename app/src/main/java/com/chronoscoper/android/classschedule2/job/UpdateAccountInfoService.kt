1ba469f99b06fe91c0864b8e8de0edf474f42573
kofu <ko.fu.dev@gmail.com>
2018-05-19 12:12:21 +0900

    Sync account info using JobService

---
+++ app/src/main/java/com/chronoscoper/android/classschedule2/job/UpdateAccountInfoService.kt
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2018 Chronoscope
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.chronoscoper.android.classschedule2.job
+
+import android.app.job.JobParameters
+import android.app.job.JobService
+import com.chronoscoper.android.classschedule2.sync.LiftimContext
+import com.chronoscoper.android.classschedule2.task.FullSyncTask
+import com.chronoscoper.android.classschedule2.task.TokenReloadTask
+
+class UpdateAccountInfoService : JobService() {
+    override fun onStopJob(params: JobParameters?): Boolean = false
+
+    override fun onStartJob(params: JobParameters?): Boolean {
+        val db = LiftimContext.getOrmaDatabase()
+        if (db.selectFromLiftimCodeInfo().count() > 0) {
+            LiftimContext.executeBackground {
+                TokenReloadTask(this).run()
+                FullSyncTask(this).run()
+            }
+        }
+        return true
+    }
+}
--- app/src/main/java/com/chronoscoper/android/classschedule2/receiver/ServiceTriggerReceiver.kt
+++ app/src/main/java/com/chronoscoper/android/classschedule2/receiver/ServiceTriggerReceiver.kt
@@ -15,48 +15,28 @@
  */
 package com.chronoscoper.android.classschedule2.receiver
 
-import android.app.AlarmManager
-import android.app.PendingIntent
+import android.app.job.JobInfo
 import android.app.job.JobScheduler
 import android.content.BroadcastReceiver
+import android.content.ComponentName
 import android.content.Context
 import android.content.Intent
-import android.os.Build
-import android.support.annotation.RequiresApi
-import com.chronoscoper.android.classschedule2.service.NotificationRegistererService
-import com.chronoscoper.android.classschedule2.util.DateTimeUtils
+import com.chronoscoper.android.classschedule2.job.UpdateAccountInfoService
 
 class ServiceTriggerReceiver : BroadcastReceiver() {
-    companion object {
-        private const val REQUEST_NOTIFICATION_REGISTERER = 101
-        private const val REQUEST_UPDATE_USER_INFO = 102
-    }
-
     override fun onReceive(context: Context?, intent: Intent?) {
         if (context == null || intent == null) return
         val action = intent.action
-        if (action == Intent.ACTION_BOOT_COMPLETED
-                || action == Intent.ACTION_MY_PACKAGE_REPLACED) {
-            val alarmManager = context.getSystemService(Context.ALARM_SERVICE) as AlarmManager
-            schedule(context, alarmManager)
+        if (action == Intent.ACTION_MY_PACKAGE_REPLACED) {
+            val info = JobInfo.Builder(1,
+                    ComponentName(context, UpdateAccountInfoService::class.java))
+                    .setPersisted(true)
+                    .setRequiredNetworkType(JobInfo.NETWORK_TYPE_UNMETERED)
+                    .setRequiresCharging(true)
+                    .setPeriodic(1000 * 60 * 60 * 24)
+                    .build()
+            val scheduler = context.getSystemService(Context.JOB_SCHEDULER_SERVICE) as JobScheduler
+            scheduler.schedule(info)
         }
-    }
-
-    private fun schedule(context: Context, alarmManager: AlarmManager) {
-        // Register service to publish notification on Info
-        val notificationPublish = PendingIntent.getService(context, REQUEST_NOTIFICATION_REGISTERER,
-                Intent(context, NotificationRegistererService::class.java),
-                PendingIntent.FLAG_CANCEL_CURRENT)
-        alarmManager.setInexactRepeating(AlarmManager.ELAPSED_REALTIME,
-                DateTimeUtils.getTomorrowPosixTime(),
-                AlarmManager.INTERVAL_DAY, notificationPublish)
-    }
-
-    @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
-    private fun isScheduled(jobScheduler: JobScheduler, jobId: Int): Boolean {
-        jobScheduler.allPendingJobs.forEach {
-            if (it.id == jobId) return true
-        }
-        return false
     }
 }
